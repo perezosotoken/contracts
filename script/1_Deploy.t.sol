f54c8beacbefd92199535503afaedd4fd9fea13a
alberthaptic <albert@haptic.finance>
2024-04-12 17:02:23 -0400

    deploy script

--- script/1_Deploy.t.sol
+++ script/1_Deploy.t.sol
@@ -2,26 +2,33 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
-import {RewardDistributor} from "../src/rewards/Distribution.sol";
+import {Distribution} from "../src/rewards/Distribution.sol";
 
 contract Deploy is Script {
     // Command to deploy:
     // forge script script/Deploy.s.sol --rpc-url=<RPC_URL> --broadcast --slow
 
     // Get environment variables.
-    address feeTo = vm.envAddress("FEE_TO");
+    // address feeTo = vm.envAddress("FEE_TO");
     uint256 privateKey = vm.envUint("PRIVATE_KEY");
     address deployer = vm.envAddress("DEPLOYER");
-    bytes32 salt = keccak256(bytes(vm.envString("SALT")));
+    // bytes32 salt = keccak256(bytes(vm.envString("SALT")));
 
-    address public perezosoToken = 0x53ff62409b219ccaff01042bb2743211bb99882e;
+    address public perezosoToken = 0x53Ff62409B219CcAfF01042Bb2743211bB99882e;
 
     function run() public {  
 
         vm.startBroadcast(privateKey);
 
-        RewardDistributor rewardDistributor = new RewardDistributor(perezosoToken);
-        
+        uint256[] memory rewardAmounts = new uint256[](2);
+
+        rewardAmounts[0] = 100_000_000e18;
+        rewardAmounts[1] = 10_000_000e18;
+
+        Distribution rewardDistributor = new Distribution(rewardAmounts, perezosoToken);
+
+        console.log("RewardDistributor deployed to address: ", address(rewardDistributor));
+
         vm.stopBroadcast();
  
     }
